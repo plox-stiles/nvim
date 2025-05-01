4ad79b103002897e31460ad581991a8f385102be
trenton stiles <stiles.trenton@gmail.com>
2025-05-01 13:09:09 -0500

    init

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,5 @@
+-- custom keybinds
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>w", ":w<CR>") -- write file
+vim.keymap.set("n", "<leader>q", ":qall!<CR>") -- force quit
+vim.keymap.set("n", "<leader>e", ":e .<CR>") -- file explore
